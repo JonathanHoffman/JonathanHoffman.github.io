64e334d1c11d78fdb4302f9d8f132c883f347910
Jonathan Hoffman <jonathanghoffman@gmail.com>
2017-09-12 22:27:05 -0500

    Fix scrolling behavior with navbar

--- js/index.js
+++ js/index.js
@@ -13,4 +13,21 @@ $(document).ready(function() {
     $('#contactSubmit').click(function () {
         sendMail();
     });
+
+    // Fix scrolling with a fixed navbar. 
+    var navOffset = $('.navbar').height();
+    
+    $('.navbar li a').click(function(event) {
+        var href = $(this).attr('href');
+    
+        // Don't let the browser scroll, but still update the current address
+        // in the browser.
+        event.preventDefault();
+        window.location.hash = href;
+    
+        // Explicitly scroll to where the browser thinks the element
+        // is, but apply the offset.
+        $(href)[0].scrollIntoView();
+        window.scrollBy(0, -navOffset);
+    });
 });
